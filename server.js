3c9b3a82f011bed0c20930724c435c39097745ad
JinJie Xu <xvx332211@hotmail.com>
2019-01-06 00:03:34 -0800

    Changed server listen

--- server.js
+++ server.js
@@ -6,6 +6,4 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
 
-server.listen(port, "0.0.0.0", () =>
-  console.log(`Listening on port ${port}...`)
-);
+server.listen(port, () => console.log(`Listening on port ${port}...`));
