1888d3fea7efdb66e853581c28168ba54c619ddb
JinJie Xu <xvx332211@hotmail.com>
2019-01-05 23:45:27 -0800

    Formatted Doc

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
-const orderRoutes = require('./api/routes/orders');
-const itemRoutes = require('./api/routes/items');
+const orderRoutes = require("./api/routes/orders");
+const itemRoutes = require("./api/routes/items");
 
 // Body parser
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -11,25 +11,25 @@ app.use(bodyParser.json());
 
 // Handle CORS
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header("Access-Control-Allow-Origin", "*");
   res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
-  if (req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE');
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE");
     return res.status(200).json({});
   }
   next();
 });
 
 // Use REST routers
-app.use('/api/orders', orderRoutes);
-app.use('/api/items', itemRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/items", itemRoutes);
 
 // Error handling for invalid requests
 app.use((req, res, next) => {
-  const err = new Error('Request Not found');
+  const err = new Error("Request Not found");
   err.status = 404;
   next(err);
 });
